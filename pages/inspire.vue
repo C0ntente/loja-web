71aed82c37385c02f038fc8a5f0430ab4584620c
Adalberto <adalbertoacarneiro@gmail.com>
2021-04-23 21:53:53 -0300

    Commit inicial

---
+++ pages/inspire.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>nome do cliente - {{ cliente.nome }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cliente: { nome: '' },
+    }
+  },
+  created() {
+    this.cliente = this.$axios.$get('/clientes/1')
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
